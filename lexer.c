d97ba2af2842c5528e081f7718b15718f3e2c2f8
Tim Rekelj <hello@timrekelj.si>
2025-02-10 21:04:54 +0100

    Start parsing the file in lexer

--- lexer.c
+++ lexer.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include "compiler.h"
 
-void lexer(char* filename) {
-    FILE *fp = fopen(filename, "r");
+typedef struct {
+    FILE* fp;
+    int line;
+    int pos;
+} File;
 
-    linfo("Filename: %s", filename);
+File create_file(char* filename) {
+    FILE* fp = fopen(filename, "r");
+
+    if (!fp) {
+        lerror("Error trying to open the file (%s)", filename);
+    }
+
+    return (File){ fp, 1, 1 };
+}
+
+char nextc(File f) {
+    char c = getc(f.fp);
+    if (c == '\r') { c = '\n'; }
+
+    if (c == '\n') {
+        f.line += 1;
+        f.pos = 1;
+    } else {
+        f.pos += 1;
+    }
+
+    return c;
+}
+
+char peek(File f) {
+    char c;
+    c = fgetc(f.fp);
+    ungetc(c, f.fp);
+    return c;
+}
+
+void lexer(char* filename) {
+    File file = create_file(filename);
+    char curr_char;
+
+    while (curr_char != EOF) {
+        curr_char = nextc(file);
+
+        if (curr_char == '\n') {
+            linfo("Character is new line");
+            linfo("Next character is: %c", peek(file));
+        } else if (curr_char == EOF) {
+            lwarn("Character is end of file.");
+        } else {
+            linfo("Char: %c", curr_char);
+        }
+    }
+
+    fclose(file.fp);
 }
